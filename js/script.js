1a17d5bf8e1384e6c9942b21a994f9a0905147dc
Charley McGowan <charley@>
2018-08-27 20:47:32 -0700

    Initial commit, will finish code next class

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var question = function(height,weight) {
+  heightLb = parseInt(prompt("Hello, please provide your height, in lbs")_;
+  weightIn = parseInt(prompt("Please provide your height, in inches"))
+  var weightKg =
+}
