6c76a2c4e5946e342ba9619c429516f2af5a9fda
charley mcgowan <charley.mcgowan@outlook.com>
2018-08-29 20:15:59 -0700

    Finished calculator, and all prompts work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-var question = function(height,weight) {
-  heightLb = parseInt(prompt("Hello, please provide your height, in lbs")_;
-  weightIn = parseInt(prompt("Please provide your height, in inches"))
-  var weightKg =
+var question = function(region) {
+  region = prompt("Would you like to calculate your BMI with the english or metric system? Please enter E or M.").toUpperCase();
+  //alert("selection: " + region)
+  if (region === "E") {
+    heightIn = parseInt(prompt("Hello, please provide your height, in inches:"));
+    weightLb = parseInt(prompt("Please provide your weight, in lbs:"));
+    var bmi = (weightLb*0.453592)/(heightIn*0.0254);
+    alert("Your BMI is " + bmi + ".");
+  }
+  else if (region === "M") {
+    var weightKg = prompt("Hello, please provide your weight in Kgs:");
+    var heightM = prompt("Please provide your height in meters:");
+    var bmi = (weightKg)/(heightM);
+    alert("Your BMI is " + bmi + ".");
+  }
+  else {
+    alert("You did not pick E or M, please pick one of those two selections for (E)nglish system, or (M)etric system");
+  }
 }
+
+question();
